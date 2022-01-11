31805250ab355d849d068dbf15833a7380c80d01
yibei <939406186@qq.com>
2022-01-12 00:32:59 +0800

    add poscar/Poscar_TF.py

--- poscar/Poscar_TF.py
+++ poscar/Poscar_TF.py
@@ -1,7 +1,13 @@
-import numpy as np
 import re
-import pandas as pd
+# Created by Junfei Ding 2022/01/12 00:23
 class Poscar_TF:
+    """
+    According to the c coordinate of the atoms to append "F F F" or "T T T"
+    to the atoms' coordinate line.
+    This requires to enter a judging condition and compare this condition to the c coordinate.
+    If the c coordinate is less this condition, "F F F" will append to this line.
+    Otherwise append "T T T" to this line
+    """
     def __init__(self, pos):
         with open(pos) as p:
             self.pos = p.readlines()
@@ -20,8 +26,13 @@ class Poscar_TF:
                 self.direct_line = i
         print(f'The D/direct line number is {self.direct_line}')
 
-    def poscar_tf(self,condition_tf):
+    def poscar_tf(self):
         # Write the lattice part to modified poscar
+        print('A judging condition is needed to decide '
+              'how to append "F F F" and "T T T" to the atom coordinate line.')
+        print("If the c value of atom's coordinate is less than this condition, "
+              "'F F F' will be append to the this line, otherwise 'T T T'.")
+        condition_tf = float(input('Now enter the judging condition: '))
         with open('POSCAR_TF','w') as poscar:
             for i in range(self.direct_line):
                 poscar.write(self.pos[i])
@@ -35,10 +46,12 @@ class Poscar_TF:
                 else:
                     self.pos[j] = self.pos[j].replace('\n','')+" T T T\n"
                 poscar.write(self.pos[j])
+        print('POSCAR_TF file has been created, check it carefully!')
 
 if __name__=='__main__':
+    print(Poscar_TF.__doc__)
     pos=Poscar_TF('POSCAR')
-    pos.poscar_tf(0.5)
+    pos.poscar_tf()
 
 
 
